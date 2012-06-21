b2cb4bd1280bb0d003dc0bd494419821857c239c
HIROSE Masaaki <hirose31@gmail.com>
2012-06-22 01:24:24 +0900

    Support more fxxking vi family

--- init.sh
+++ init.sh
@@ -15,7 +15,27 @@ nemacs
 mg
 "
 
+VI="
+vim
+nvi
+jelvis
+elvis
+vi
+ex
+ed
+"
+
 echo "powerup your emacs."
+
+POWERFUL_EDITOR='cat >/dev/null'
+for pe in $VI;
+do
+    if type $pe 2>/dev/null | grep 'is /' >/dev/null 2>&1;
+    then
+        POWERFUL_EDITOR=$pe
+        break
+    fi
+done
 
 for FILE in ${RCFILES};
 do
@@ -28,12 +48,12 @@ do
             if [ ${FILE} = ".cshrc" -o ${FILE} = ".tcshrc" ]; then
                 for e in $EMACSEN;
                 do
-                    echo "alias $e 'vim'"
+                    echo "alias $e '$POWERFUL_EDITOR'"
                 done
             else
                 for e in $EMACSEN;
                 do
-                    echo "alias $e='vim'"
+                    echo "alias $e='$POWERFUL_EDITOR'"
                 done
             fi
         } >> ~/${FILE}
